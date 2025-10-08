39b417190309983afccee7def794021994d473d5
GabrielNNS <gabriel.nascimento33@hotmail.com>
2025-10-08 08:50:19 -0300

    initial/create-project

---
+++ src/test/java/com/gns/moneyflow_api/MoneyflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gns.moneyflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
